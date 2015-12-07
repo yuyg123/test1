319b23dca1a6c5e6daf7446f240509b5fd6b81ac
John <yuyg@succez.com>
2015-12-07 14:30:33 +0800

    10进制转16进制

--- src/test1/InttoH.java
+++ src/test1/InttoH.java
@@ -5,7 +5,8 @@ public class InttoH {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(Integer.toHexString(10)); 
+		System.out.println(Integer.toHexString(10));
+		System.out.println(Integer.toHexString(11));
 
 	}
 	
